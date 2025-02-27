b7fdb3e167d33d3a68f1e95563e14b43b11b6338
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-02-27 15:32:58 +0530

    TryLock and Unlock

--- src/UsingLock/BankAcc.java
+++ src/UsingLock/BankAcc.java
@@ -1,12 +1,18 @@
 package UsingLock;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAcc {
     private int balance = 100;
 
-    public synchronized void withdraw(int amount) {
+    private Lock lock = new ReentrantLock();
+
+    public void withdraw(int amount) {
         System.out.println(Thread.currentThread().getName() + " attempting to withdraw " + amount);
 
-        if(balance>= amount) {
+        /*if(balance>= amount) {
             System.out.println(Thread.currentThread().getName() + " Proceeding with withdrawals ");
             try {
                 Thread.sleep(3000);
@@ -16,6 +22,30 @@ public class BankAcc {
         }
         else {
             System.out.println(Thread.currentThread().getName() + " Insufficient balance ");
+        }*/
+        //commented to implement explicit lock now
+
+        //EXPLICIT LOCK -
+        try {
+            if (lock.tryLock(1000, TimeUnit.MILLISECONDS)) {
+                if(balance>= amount) {
+                    System.out.println(Thread.currentThread().getName() + " Proceeding with withdrawals ");
+                    try {
+                        Thread.sleep(1500);
+                    } catch (InterruptedException e) {}
+                    finally {
+                        lock.unlock();
+                    }
+                    balance -= amount;
+                    System.out.println(Thread.currentThread().getName() + " Remaining balance after withdrawals " + balance);
+                }
+                else {
+                    System.out.println(Thread.currentThread().getName() + " Insufficient balance ");
+                }
+            } else {
+                System.out.println(Thread.currentThread().getName() +" Could not acquire the lock, try later ");
+            }
+        } catch (InterruptedException e) {
         }
     }
 }
