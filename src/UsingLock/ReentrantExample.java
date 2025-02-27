1eedbbe3251777c693c4a6d83624abac8585d44f
Chahat-23 <chahatshrivastava230620@gmail.com>
2025-02-27 15:40:22 +0530

    Synchronized, Explicit Locking, and Reentrant Lock

---
+++ src/UsingLock/ReentrantExample.java
@@ -0,0 +1,32 @@
+package UsingLock;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantExample {
+    private final Lock lock = new ReentrantLock();
+   //reentrant means inner will acquire the lock without having the deadlock condition where inner and outer both are waiting for each other
+    public void outerMethod() {
+        lock.lock();
+        try {
+            System.out.println("Outer Method");
+            innerMethod();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void innerMethod() {
+        lock.lock();
+        try {
+            System.out.println("Inner Method");
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        ReentrantExample example = new ReentrantExample();
+        example.outerMethod();
+    }
+}
